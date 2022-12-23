54d2e5d32bca527ffb6f6b4f174a8ed61695a753
Stanley643 <nmurigi@kabarak.ac.ke>
2022-12-22 19:00:55 -0800

    pointers

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+
+ * main - check the code for Holberton School students.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
+{
+  
+  char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+  
+  char *p;
+
+printf("%s", p);
+printf("%s", s);
+return (0);
+}
