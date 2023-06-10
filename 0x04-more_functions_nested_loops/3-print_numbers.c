7aa408d711c6db3900c4df6edce73b2198952e4d
Stanley643 <nmurigi@kabarak.ac.ke>
2023-06-10 03:02:48 -0700

    print fn

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers -prints out
+ * void: data type not defined
+ * Return: output
+ *
+ */
+
+void print_number(void)
+{
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
