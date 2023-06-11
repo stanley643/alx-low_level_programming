68dea0a6e11b30a8abfb3b31f4b82a78869dc890
Stanley643 <nmurigi@kabarak.ac.ke>
2023-06-11 07:16:52 -0700

    cxn

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,14 @@
 #include "main.h"
-
+/**
+ * print_line - fns
+ * @n: number
+ */
 void print_line(int n)
 {
 	int i;
 	char c = '_';
 
-	if (n>0)
+	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
 		{
