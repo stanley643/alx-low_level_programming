a57e0b6d73a00e73025bc872317a0c4b8442e7c4
Stanley643 <nmurigi@kabarak.ac.ke>
2023-07-04 05:49:49 -0700

    counting the number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint- the function of printing the values of a node
+ * h: pointer been passed as value of head
+ * Return: NOTHING
+ *
+ */
+size_t listint_len(const listint_t *h)
+{
+        int count;
+        const listint_t *temp = NULL;
+        temp = h;
+
+        while (temp != NULL)
+        {
+                count++;
+                temp = temp->next;
+        }
+        return (count);
+}
