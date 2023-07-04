7914c7ddb42893a6718be1e8cc59911a9bafcca6
Stanley643 <nmurigi@kabarak.ac.ke>
2023-07-04 06:39:07 -0700

    add a node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,24 +1,24 @@
 #include "lists.h"
 
 /**
- * print_listint- the function of printing the values of a node
- * h: pointer been passed as value of head
- * Return: NOTHING
+ * add_nodeint- the function of adding the values of a node at the beginning
+ * @head: pointer been passed as value of head
+ * @n: integer been passed
+ * Return: returns NULL and the head
  *
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-        listint_t *temp = malloc(sizeof(listint_t));
+	listint_t *temp = malloc(sizeof(listint_t));
+
 	if (temp == NULL)
 	{
 		return (NULL);
 	}
-	
 	temp->n = n;
 	temp->next = *head;
 
 	*head = temp;
 
 	return (*head);
-        
 }
