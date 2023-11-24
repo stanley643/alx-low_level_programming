980e24d3c98301ccd048d1e2f8eebdce4eb13051
Stanley643 <nmurigi@kabarak.ac.ke>
2023-11-24 11:33:42 +0300

    bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,12 @@
-#include "main.c"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 void print_binary(unsigned long int n)
 {
-	if (n == NULL)
-		return 0;
-	else
-	{
 	if (n > 1)
 	{
 		print_binary(n >> 1);
-	}
 	}
 	putchar ((n & 1) + '0');
 }
