3d466dbb6ea7705bc2fdf608dbb5bcc6a7a79586
stanley643 <nmurigi@kabarak.ac.ke>
2024-06-17 10:00:02 -0700

    apart from printf and puts or any other functions that they share the man page we can use write to perform the same function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<string.h>
+#include<unistd.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t message_len = strlen(message);
+
+	write(STDERR_FILENO, message, message_len);
+	return 1;
+}
